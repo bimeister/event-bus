72ecd3d955021cd1b7f4b3eb38ae77abab69aa64
Fedor Usakov <fedor.usakov@bimeister.com>
2021-12-08 13:37:33 +0300

    test: add perfomance testing

--- src/lib/event-bus.spec.ts
+++ src/lib/event-bus.spec.ts
@@ -1,21 +1,37 @@
 import { getShuffledArray } from '@bimeister/utilities';
+import { performance, PerformanceEntry, PerformanceObserver } from 'perf_hooks';
 import type { Listener } from '../internal/classes/listener.class';
+import { MeasureObserver } from '../internal/classes/measure-observer.class';
 import { WrappedEvent } from '../internal/classes/wrapped-event.class';
 import { VOID } from '../internal/constants/void.const';
 import { PayloadType } from '../internal/enums/payload-type.enum';
 import { EventBus } from './event-bus.class';
 
+const enum PERFORMANCE_MARK {
+  Start = 'Start',
+  End = 'End'
+}
+
 describe('event-bus.class.ts', () => {
   let eventBus: EventBus;
-  let shuffledArray: number[];
+  let shuffledArray1K: number[];
+  let shuffledArray1M: number[];
 
   beforeAll(() => {
     eventBus = new EventBus();
-    shuffledArray = getShuffledArray(new Array(1000).fill(VOID).map((_item: void, index: number) => index));
+    shuffledArray1K = getShuffledArray(new Array(1000).fill(VOID).map((_item: void, index: number) => index));
+    shuffledArray1M = new Array(1_000_000 / shuffledArray1K.length).fill(VOID).reduce((accumulatedValue: number[]) => {
+      accumulatedValue.push(...shuffledArray1K);
+      return accumulatedValue;
+    }, []);
+  });
+
+  beforeEach(() => {
+    performance.clearMarks();
   });
 
   it('should handle dispatched events in valid sequence', (doneCallback: jest.DoneCallback) => {
-    const input: number[] = shuffledArray;
+    const input: number[] = shuffledArray1K;
     const output: unknown[] = [];
 
     eventBus.listen((event: unknown, listener: Listener) => {
@@ -33,8 +49,8 @@ describe('event-bus.class.ts', () => {
     input.forEach((inputItem: number) => eventBus.dispatch(inputItem));
   }, 10_000);
 
-  it('should handle data with EventWrapper if configured', (doneCallback: jest.DoneCallback) => {
-    const payload: number[] = shuffledArray;
+  it('should handle wrapped data (PayloadType.Wrapped)', (doneCallback: jest.DoneCallback) => {
+    const payload: number[] = shuffledArray1K;
 
     eventBus.listen(
       (wrappedEvent: WrappedEvent<unknown>, listener: Listener) => {
@@ -52,8 +68,8 @@ describe('event-bus.class.ts', () => {
     eventBus.dispatch(payload);
   }, 10_000);
 
-  it('should handle data without EventWrapper if configured', (doneCallback: jest.DoneCallback) => {
-    const payload: number[] = shuffledArray;
+  it('should handle unwrapped data (PayloadType.Native)', (doneCallback: jest.DoneCallback) => {
+    const payload: number[] = shuffledArray1K;
 
     eventBus.listen(
       (unWrappedEvent: unknown, listener: Listener) => {
@@ -68,5 +84,60 @@ describe('event-bus.class.ts', () => {
     );
 
     eventBus.dispatch(payload);
+  }, 10_000);
+
+  it('should dispatch 100 million events faster then 1s (bulk)', (doneCallback: jest.DoneCallback) => {
+    const measureName: string = 'Dispatch measure (bulk)';
+
+    new MeasureObserver(measureName).observe((observer: PerformanceObserver, targetEntry: PerformanceEntry) => {
+      expect(targetEntry.duration).toBeLessThan(1000);
+      observer.disconnect();
+      doneCallback();
+    });
+
+    const payload: number[] = shuffledArray1M;
+
+    performance.mark(PERFORMANCE_MARK.Start);
+    eventBus.dispatch(payload);
+    performance.mark(PERFORMANCE_MARK.End);
+
+    performance.measure(measureName, PERFORMANCE_MARK.Start, PERFORMANCE_MARK.End);
+  }, 10_000);
+
+  it('should dispatch 1 million events faster then 1.5s (sequence)', (doneCallback: jest.DoneCallback) => {
+    const measureName: string = 'Dispatch measure (sequence)';
+
+    new MeasureObserver(measureName).observe((observer: PerformanceObserver, targetEntry: PerformanceEntry) => {
+      expect(targetEntry.duration).toBeLessThan(1500);
+      observer.disconnect();
+      doneCallback();
+    });
+
+    const payload: number[] = shuffledArray1M;
+
+    performance.mark(PERFORMANCE_MARK.Start);
+    payload.forEach((item: number) => eventBus.dispatch(item));
+    performance.mark(PERFORMANCE_MARK.End);
+
+    performance.measure(measureName, PERFORMANCE_MARK.Start, PERFORMANCE_MARK.End);
+  }, 10_000);
+
+  it('should dispatch 1 event and catch it faster then 3ms', (doneCallback: jest.DoneCallback) => {
+    const measureName: string = 'Dispatch+Catch measure';
+
+    new MeasureObserver(measureName).observe((observer: PerformanceObserver, targetEntry: PerformanceEntry) => {
+      expect(targetEntry.duration).toBeLessThan(3);
+      observer.disconnect();
+      doneCallback();
+    });
+
+    const payload: number = NaN;
+
+    performance.mark(PERFORMANCE_MARK.Start);
+    eventBus.listen((_response: unknown, listener: Listener) => listener.stop());
+    eventBus.dispatch(payload);
+    performance.mark(PERFORMANCE_MARK.End);
+
+    performance.measure(measureName, PERFORMANCE_MARK.Start, PERFORMANCE_MARK.End);
   }, 10_000);
 });
