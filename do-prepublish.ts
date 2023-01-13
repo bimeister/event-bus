8942430ca4d6aa0f4824fe6b403cdf7c5f5c7107
progikusok <moyomylo11@gmail.com>
2023-01-13 11:10:45 +0300

    build: add ci/cd workflows

--- do-prepublish.ts
+++ do-prepublish.ts
@@ -1,71 +1,28 @@
 import { env } from 'process';
-import { copyFile, readFile, writeFile } from 'fs/promises';
+import { copyFile, mkdir, readFile, writeFile } from 'fs/promises';
+import { isEmpty, isNil } from '@bimeister/utilities';
+import { existsSync } from 'fs';
 import { join } from 'path';
-import { isEmpty } from '@bimeister/utilities';
 
-const NPM_AUTH_TOKEN: string = `${env.AUTH_TOKEN}`;
-const GIT_COMMIT_HASH: string = `${env.GIT_COMMIT_HASH}`;
+const IS_DEV_PUBLISH: boolean = Boolean(env.IS_DEV_PUBLISH);
+const GIT_COMMIT_HASH: string | undefined = env.GIT_COMMIT_HASH;
 const CURRENT_LOCATION: string = `${__dirname}`;
+const VERSION: string | undefined = env.VERSION;
 
-async function createNpmRc(): Promise<void> {
-  const npmrcPath: string = './.npmrc';
-  const authToken: string = `${NPM_AUTH_TOKEN}`;
-  const orgEmail: string = 'info@bimeister.com';
-  const registry: string = `https://npm.pkg.github.com/:_authToken=${{ authToken }}`;
-
-  const npmRcPath: string = join(CURRENT_LOCATION, npmrcPath);
-  const npmRcContentLines: string[] = await readFile(npmRcPath, 'utf-8').then((content: string) =>
-    content.split(/\r?\n/)
-  );
-
-  const currentContentEntries: [string, string][] = npmRcContentLines.map((line: string): [string, string] => {
-    const dividedLine: string[] = line.split('=');
-
-    const invalidKeyValuePair: boolean = dividedLine.length !== 2;
-    if (invalidKeyValuePair) {
-      return ['', ''];
-    }
-
-    const [rawKey, rawValue]: string[] = dividedLine;
-    const key: string = String(rawKey).trim();
-    const value: string = String(rawValue).trim();
-
-    return [key, value];
-  });
-
-  const currentContentValueByKey: Map<string, string> = new Map<string, string>(currentContentEntries);
-
-  currentContentValueByKey.set('_auth', authToken);
-  currentContentValueByKey.set('email', orgEmail);
-
-  const sourceRegistryWithTrailingSlash: string = registry.endsWith('/') ? registry : `${registry}/`;
-  currentContentValueByKey.set('registry', sourceRegistryWithTrailingSlash);
-  const processedSourceRegistry: string = sourceRegistryWithTrailingSlash
-    .replace('http://', '')
-    .replace('https://', '');
-  currentContentValueByKey.set(`//${processedSourceRegistry}:_authToken`, authToken);
-
-  currentContentValueByKey.forEach((_: string, key: string) => {
-    const isScopedRegistry: boolean = key.startsWith('@');
-    if (!isScopedRegistry) {
-      return;
-    }
-    currentContentValueByKey.delete(key);
-  });
-
-  const updatedContent: string = Array.from(currentContentValueByKey.entries())
-    .map(([key, value]: [string, string]) => `${key} = ${value}`)
-    .join('\n');
-
-  await writeFile(`${CURRENT_LOCATION}/dist/${npmrcPath}`, updatedContent);
+function createDistFolder(): Promise<string | undefined> {
+  return mkdir('./dist', { recursive: true });
 }
 
 async function createPackageJson(): Promise<void> {
-  const commitHash: string = `${GIT_COMMIT_HASH}`;
-  const packageJsonPath: string = './dist/package.json';
+  const packageJsonPath: string = join('dist', 'package.json');
+  const targetPackageJsonPath: string = join(CURRENT_LOCATION, packageJsonPath);
 
-  const currentContent: object = await readFile(`${CURRENT_LOCATION}/${packageJsonPath}`, 'utf-8').then(
-    (content: string) => (isEmpty(content) ? {} : JSON.parse(content))
+  if (!existsSync(targetPackageJsonPath)) {
+    throw new Error(`Looks like you forgot run build first. Path ${targetPackageJsonPath} does not exist.`);
+  }
+
+  const currentContent: object = await readFile(targetPackageJsonPath, 'utf-8').then((content: string) =>
+    Boolean(isEmpty(content)) ? {} : JSON.parse(content)
   );
   const currentContentEntries: [string, unknown][] = Object.entries(currentContent);
 
@@ -73,18 +30,26 @@ async function createPackageJson(): Promise<void> {
     currentContentEntries.map(([key, value]: [string, unknown]) => [key, value])
   );
 
-  const currentProperVersion: unknown = contentValueByKey.get('version');
-  const updatedProperVersion: string = `${currentProperVersion}-${commitHash}`;
+  if (isNil(VERSION)) {
+    throw new Error('Package.json version is invalid');
+  }
+
+  const metadataSuffix: string = IS_DEV_PUBLISH ? 'dev' : 'stable';
+  const updatedProperVersion: string = isNil(GIT_COMMIT_HASH)
+    ? VERSION
+    : `${VERSION}-${metadataSuffix}.sha.${GIT_COMMIT_HASH.slice(0, 8)}`;
+
   contentValueByKey.set('version', updatedProperVersion);
 
   const updatedContent: object = Object.fromEntries(contentValueByKey.entries());
 
-  await writeFile(`${CURRENT_LOCATION}/${packageJsonPath}`, JSON.stringify(updatedContent));
+  return writeFile(targetPackageJsonPath, JSON.stringify(updatedContent));
 }
 
 Promise.resolve()
-  .then(() => copyFile('./.npmrc', './dist/.npmrc'))
+  .then(() => createDistFolder())
   .then(() => copyFile('./.npmignore', './dist/.npmignore'))
-  .then(() => copyFile('./LICENSE', './dist/LICENSE'))
-  .then(() => createNpmRc())
-  .then(() => createPackageJson());
+  .then(() => copyFile('./README.md', './dist/README.md'))
+  .then(() => copyFile('./LICENSE.md', './dist/LICENSE.md'))
+  .then(() => createPackageJson())
+  .catch((error: unknown) => console.warn(error));
